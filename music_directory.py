a5ae25965b9efdbf1a860456b345617b8d557c5c
Ghostkeeper <rubend@tutanota.com>
2022-05-28 23:58:12 +0200

    List files in pointed-to directory
    Like, the main functionality of this model. Get the files.
    The metadata is placeholder for now.

--- music_directory.py
+++ music_directory.py
@@ -4,10 +4,9 @@
 # This application is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 # You should have received a copy of the GNU Affero General Public License along with this application. If not, see <https://gnu.org/licenses/>.
 
+import os
 import PyQt6.QtCore
 import typing
-
-import track
 
 class MusicDirectory(PyQt6.QtCore.QAbstractListModel):
 	"""
@@ -30,6 +29,7 @@ class MusicDirectory(PyQt6.QtCore.QAbstractListModel):
 			b"bpm": user_role + 5
 		}
 		self._directory = ""
+		self._data = []
 
 	def directory_set(self, new_directory) -> None:
 		"""
@@ -37,6 +37,7 @@ class MusicDirectory(PyQt6.QtCore.QAbstractListModel):
 		:param new_directory: A path to a directory to look at.
 		"""
 		self._directory = new_directory
+		self.update()
 
 	@PyQt6.QtCore.pyqtProperty(str, fset=directory_set)
 	def directory(self) -> str:
@@ -55,7 +56,39 @@ class MusicDirectory(PyQt6.QtCore.QAbstractListModel):
 	def data(self, index, role) -> typing.Any:
 		return self._data[index.row()][role]
 
-	"""
-	Triggered when the list of tracks changes.
-	"""
-	tracksChanged = PyQt6.QtCore.pyqtSignal()
+	def update(self) -> None:
+		"""
+		Update the track list from the current contents of the watched directory.
+		"""
+		file_role = self.roles[b"filepath"]
+		files = set(os.listdir(self._directory))
+		old_files = set()
+		for item in self._data:
+			old_files.add(item[file_role])
+		new_files = files - old_files
+		removed_files = old_files - files
+
+		for removed_file in removed_files:
+			remove_pos = 0
+			for i in range(len(self._data)):
+				if self._data[i][file_role] == removed_file:
+					remove_pos = i
+			self.beginRemoveRows(PyQt6.QtCore.QModelIndex(), remove_pos, remove_pos)
+			del self._data[remove_pos]
+			self.endRemoveRows()
+
+		for new_file in new_files:
+			insert_pos = len(self._data)
+			for i in range(len(self._data)):
+				if self._data[i][file_role] < new_file:
+					insert_pos = i
+					break
+			self.beginInsertRows(PyQt6.QtCore.QModelIndex(), insert_pos, insert_pos)
+			self._data.insert(insert_pos, {
+				self.roles[b"filepath"]: new_file,
+				self.roles[b"title"]: "Test title",
+				self.roles[b"author"]: "Test author",
+				self.roles[b"duration"]: 180.0,
+				self.roles[b"bpm"]: 140.0
+			})
+			self.endInsertRows()
