9f0ac16e6050f31ced933986a4da58757e7e06f4
Ghostkeeper <rubend@tutanota.com>
2022-09-24 16:58:56 +0200

    Implement starting and stopping the music
    This now officially turns this application into a barely-useable music player. The main functionality is in place.

--- player.py
+++ player.py
@@ -8,6 +8,8 @@ import logging
 import pygame  # The media player we're using to play music.
 import PySide6.QtCore  # Exposing the player to QML.
 
+import preferences  # To get the playlist.
+
 class Player(PySide6.QtCore.QObject):
 	"""
 	An object that is responsible for playing music files, controlling how they're played (pause, play, volume,
@@ -23,10 +25,22 @@ class Player(PySide6.QtCore.QObject):
 	Dict mapping paths to sound files to pre-loaded sound objects in Pygame.
 	"""
 
-	is_playing = False
+	current_track = None
 	"""
-	Whether the music currently is (or should be) playing.
+	If a song is playing, this holds the currently playing track.
+
+	If no song is playing, this is ``None``.
 	"""
+
+	def __init__(self, parent=None) -> None:
+		"""
+		Ensures that a few global things are properly initialised before using this class.
+		:param parent: A parent Qt Object that this object is a child of.
+		"""
+		super().__init__(parent)
+		prefs = preferences.Preferences.getInstance()
+		if not prefs.has("player/fadeout"):
+			prefs.add("player/fadeout", 2.0)  # Fade-out for 2 seconds by default.
 
 	def preload(self, path) -> None:
 		"""
@@ -59,9 +73,17 @@ class Player(PySide6.QtCore.QObject):
 		Start or stop the music.
 		:param new_is_playing: Whether the music should be playing or not.
 		"""
-		self.is_playing = new_is_playing
+		prefs = preferences.Preferences.getInstance()
+		if self.current_track is None and new_is_playing:
+			next_song = prefs.get("playlist/playlist")[0]["path"]
+			logging.info(f"Starting playback of track: {next_song}")
+			self.current_track = self.sounds[next_song]
+			self.current_track.play()
+		elif self.current_track is not None and not new_is_playing:
+			logging.info(f"Stopping playback.")
+			self.current_track.fadeout(round(prefs.get("player/fadeout") * 1000))  # Fade-out, convert to milliseconds for Pygame.
+			self.current_track = None
 		self.is_playing_changed.emit()
-		# TODO: Start/stop the music.
 
 	@PySide6.QtCore.Property(bool, fset=is_playing_set, notify=is_playing_changed)
 	def isPlaying(self) -> bool:
@@ -72,4 +94,4 @@ class Player(PySide6.QtCore.QObject):
 		playing.
 		:return: ``True`` if the music is currently playing, or ``False`` if it is stopped.
 		"""
-		return self.is_playing
+		return self.current_track is not None
