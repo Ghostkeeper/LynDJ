1e479115e6c10d05f804340a6f209553039628fe
Ghostkeeper <rubend@tutanota.com>
2023-01-06 13:50:41 +0100

    Only add background task if there is no Fourier yet
    Otherwise the progress bar will be very inaccurate, and it'll also take 5-10 minutes to get through the data even if all of the Fourier images are already generated, since it'll wait a second between each task.

--- music_directory.py
+++ music_directory.py
@@ -210,7 +210,9 @@ class MusicDirectory(PySide6.QtCore.QAbstractTableModel):
 
 		# Add background tasks for caching Fourier images.
 		def cache_fourier(path):
-			player.Player.get_instance().load_and_generate_fourier(path)
+			fourier_file = metadata.get(path, "fourier")
+			if fourier_file == "" or not os.path.exists(fourier_file):  # Not generated yet.
+				player.Player.get_instance().load_and_generate_fourier(path)
 		tasks = background_tasks.BackgroundTasks.get_instance()
 		for path in files:
 			tasks.add(lambda p=path: cache_fourier(p))
