736935fc279bda258c92e126dbb19f3e8a6c28c4
Ghostkeeper <rubend@tutanota.com>
2023-01-10 20:32:19 +0100

    Rename light_deco theme to LightDeco
    I'm not going to bother with making a field in the theme's JSON for the human-readable name, and then getting the theme names from each of the themes' JSON files. Let's just use the directory name for now.

--- theme.py
+++ theme.py
@@ -1,5 +1,5 @@
 # Music player software aimed at Lindy Hop DJs.
-# Copyright (C) 2022 Ghostkeeper
+# Copyright (C) 2023 Ghostkeeper
 # This application is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # This application is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 # You should have received a copy of the GNU Affero General Public License along with this application. If not, see <https://gnu.org/licenses/>.
@@ -41,7 +41,7 @@ class Theme(PySide6.QtCore.QObject):
 	def __init__(self):
 		super().__init__(None)
 
-		preferences.Preferences.getInstance().add("theme", "light_deco")
+		preferences.Preferences.getInstance().add("theme", "LightDeco")
 
 		self.sizes = {}
 		self.colours = {}
