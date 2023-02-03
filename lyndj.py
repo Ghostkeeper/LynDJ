b33bfed02586fc48060f554749df269320419d1d
Ghostkeeper <rubend@tutanota.com>
2023-02-03 14:30:28 +0100

    Call upon application from new package

--- lyndj.py
+++ lyndj.py
@@ -1,5 +1,5 @@
 # Music player software aimed at Lindy Hop DJs.
-# Copyright (C) 2022 Ghostkeeper
+# Copyright (C) 2023 Ghostkeeper
 # This application is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # This application is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 # You should have received a copy of the GNU Affero General Public License along with this application. If not, see <https://gnu.org/licenses/>.
@@ -8,7 +8,7 @@ import logging
 import signal  # To catch interrupts that need to stop the application.
 import sys  # Return correct exit code.
 
-import application
+import lyndj.application
 
 """
 Entry point for the application. This starts a QtApplication.
@@ -19,5 +19,5 @@ if __name__ == "__main__":
 	logging.basicConfig(format="%(levelname)s:%(asctime)s | %(message)s", level=logging.INFO)
 
 	signal.signal(signal.SIGINT, signal.SIG_DFL)  # Python is not handling SIGINT, so let the kernel do that.
-	app = application.Application(sys.argv)
+	app = lyndj.application.Application(sys.argv)
 	sys.exit(app.exec())
