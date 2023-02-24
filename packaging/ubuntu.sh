ae1ae977de014877259ce9f43da9032a9c006a0c
Ghostkeeper <rubend@tutanota.com>
2023-02-24 08:14:13 +0100

    Fix space at if statement
    Apparently this matters on Debian but not on Ubuntu. Weird.

--- packaging/ubuntu.sh
+++ packaging/ubuntu.sh
@@ -8,7 +8,7 @@
 # Tested using Ubuntu 22.04 and Debian 11.
 
 # Install dependencies.
-if [ $(dpkg-query -W -f='${Status}' python3-pip 2>/dev/null | grep -c "ok installed") -eq 0 && $(dpkg-query -W -f='${Status}' portaudio19-dev 2>/dev/null | grep -c "ok installed") -eq 0 && $(dpkg-query -W -f='${Status}' libfuse2 2>/dev/null | grep -c "ok installed") -eq 0]; then
+if [ $(dpkg-query -W -f='${Status}' python3-pip 2>/dev/null | grep -c "ok installed") -eq 0 && $(dpkg-query -W -f='${Status}' portaudio19-dev 2>/dev/null | grep -c "ok installed") -eq 0 && $(dpkg-query -W -f='${Status}' libfuse2 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
 	sudo apt update
 	sudo apt install python3-pip portaudio19-dev libfuse2
 fi
