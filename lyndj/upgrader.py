fb14bf05632f5a774be885a44b9d579ba2d87871
Ghostkeeper <rubend@tutanota.com>
2023-03-01 10:26:12 +0100

    Only check version number in preferences for being too modern
    This simplifies the process a bit.

--- lyndj/upgrader.py
+++ lyndj/upgrader.py
@@ -6,12 +6,10 @@
 
 import json  # To parse the preferences file.
 import logging
-import os.path  # To find the configuration files.
-import sqlite3  # To parse the metadata database.
+import os.path  # To find the version number.
 import sys  # To quit if the configuration is too modern.
-import typing
 
-import lyndj.storage  # To find the configuration files.
+import lyndj.storage  # To find the version number.
 
 class Upgrader:
 	"""
@@ -25,22 +23,8 @@ class Upgrader:
 		"""
 		Starting point for the upgrading of the application's configuration directories.
 		"""
-		# For logging which parts of the configuration are deemed to modern to use.
-		# Each entry is a tuple of the type of configuration and its version number.
-		too_modern = []  # type: typing.List[typing.Tuple[str, int]]
-
-		# Since the configuration files have never changed yet in the history of this application, we only need to check for files that are too modern for us to understand.
-		# If those files are present, we may not parse them properly and may run into errors.
-		# So we simply abort the application then.
-		metadata_path = os.path.join(lyndj.storage.data(), "metadata.db")
-		if os.path.exists(metadata_path):
-			connection = sqlite3.connect(metadata_path)
-			version_nr = connection.execute("PRAGMA user_version").fetchone()[0]
-			if version_nr > 0:
-				too_modern.append(("metadata", version_nr))
-				logging.error(f"The metadata database is too modern for this version of the application! Version {version_nr}.")
-		# lyndj.log is not checked.
-
+		# Since the configuration files have never changed yet in the history of this application, we just need to check if the configuration is more modern than what this version understands.
+		# If it is present and too modern, we may not parse it properly and run into errors.
 		preferences_path = os.path.join(lyndj.storage.config(), "preferences.json")
 		if os.path.exists(preferences_path):
 			with open(preferences_path) as f:
@@ -48,31 +32,15 @@ class Upgrader:
 					prefs = json.load(f)
 					version_nr = prefs["version"]
 					if version_nr != "1.0.0":
-						too_modern.append(("preferences", version_nr))
 						logging.error(f"The preferences file is too modern for this version of the application! Version {version_nr}.")
+						self.report_too_modern(version_nr)
 				except json.JSONDecodeError:
-					too_modern.append(("preferences", -1))  # Unknown version, was corrupt? Or unintelligible.
 					logging.error("The preferences file is not intelligible to this version of the application! Couldn't find the version number.")
+					self.report_too_modern("unknown")
 
-		fourier_path = os.path.join(lyndj.storage.cache(), "fourier", "version.txt")
-		if os.path.exists(fourier_path):
-			with open(fourier_path) as f:
-				try:
-					version_nr = int(f.read())
-					if version_nr > 0:
-						too_modern.append(("fourier", version_nr))
-						logging.error(f"The Fourier images are too modern for this version of the application! Version {version_nr}.")
-				except ValueError:
-					too_modern.append(("fourier", -1))  # Unknown version, was corrupt? Or unintelligible.
-					logging.error("The Fourier images version file is not intelligible to this version of the application! Couldn't find the version number.")
-
-		if too_modern:
-			self.report_too_modern(too_modern)
-
-	def report_too_modern(self, modern_parts: typing.List[typing.Tuple[str, int]]) -> None:
+	def report_too_modern(self, version_nr: str) -> None:
 		"""
 		Report to the user that parts of the configuration are too modern.
-		:param modern_parts: What parts of the configuration are too modern. Could be used to show more information to
-		the user.
+		:param version_nr: The version number found. This would be a newer version than this application.
 		"""
 		sys.exit()  # For now, simply give up.
